cda9390d8b79c20c7916bcb4cf778c824d3d81ac
vgaurav-umich <vgaurav@umich.edu>
2022-03-05 23:59:20 -0500

    Removed Data folder from Git.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name='market_watch',
-    packages=find_packages(),
+    packages=find_packages(exclude=('test*','testing*')),
     version='0.1.0',
     description='Capstone project for UMSI MADS program. RL bot for stock trading.',
     author='Matt, Egor, and Gaurav',
