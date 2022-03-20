fceaf28adfe20d8201bfc520b8dd4f71531ab460
vgaurav-umich <vgaurav@umich.edu>
2022-03-20 16:07:39 -0400

    rebased onto merge branch

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name='market_watch',
-    packages=find_packages(exclude=('test*', 'testing*')),
+    packages=find_packages(exclude=('test*','testing*')),
     version='0.1.0',
     description='Capstone project for UMSI MADS program. RL bot for stock trading.',
     long_description='Capstone project for UMSI MADS program. RL bot for stock trading.',
@@ -10,12 +10,10 @@ setup(
     author='Matt, Egor, and Gaurav',
     license='MIT',
     install_requires=[
-        'ploomber',
-        'jupyterlab',
-        'pipenv'
     ],
     setup_requires=[
-
+        'ploomber',
+        'pipenv'
     ],
     tests_require=['pytest', 'pytest-flask'],
 )
