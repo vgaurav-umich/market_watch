edaae4415c5493328145ee064b985ce99233d47d
matt-dann <mattdann@umich.edu>
2022-04-12 08:16:28 -0400

    created interim file saving for fetch_n_filter_gdelt_bq

--- src/data/fetch_n_filter_gdelt_bq.py
+++ src/data/fetch_n_filter_gdelt_bq.py
@@ -27,7 +27,7 @@ from tqdm import tqdm
 import re
 import collections
 import json
-
+import datetime
 import os
 import sys
 import warnings
@@ -40,6 +40,7 @@ from src import utils
 # alt.themes.enable('fivethirtyeight')
 
 warnings.filterwarnings('ignore')
+
 
 # + tags=[]
 load_dotenv(find_dotenv('market_watch.env'))
@@ -110,9 +111,12 @@ start_date = utils.gdelt_date_format(start_date)
 query = build_gdelt_query(table_name, search_term, start_date)
 # counter_query= build_gdelt_query_for_counter(table_name, start_date)
 # print(query)
+# -
+
+client = bigquery.Client()
 
 # + tags=[]
-client = bigquery.Client()
+
 data_df = fetch_data(client, query)
 print(f"Processed merged file with {len(data_df)} records")
 
@@ -126,6 +130,11 @@ del data_df
 
 # + tags=[]
 def create_increments(start_date):
+    '''
+    Helper function that builds 2day increments to be passed to the GDELT Query.
+    This is needed because the fetch counter data function uses a query that would 
+    be too large without chunking by dates.
+    '''
     dt= pd.to_datetime(start_date)
     range_= pd.date_range(dt, end= datetime.date.today(), freq='2D')
     string_range= []
@@ -138,7 +147,13 @@ def create_increments(start_date):
     return list(date_range)
 
 def get_rel_company_names(path):
+    '''
+    Helper function that takes the stock names and adds variations to the name. 
+    Example: American Airlines Group Inc. becomes a dict that includes: 
+    American, American Airlines, American Airlines Group, & American Airlines Group Incorporated.
+    '''
     rel_company = pd.read_csv(path)
+    rel_company = rel_company[rel_company['Market Cap'] >= 100000000]
     rel_company = rel_company['Name']
     rel_company = rel_company.str.lower()
     rel_company = rel_company[rel_company.str.contains('common|ordinary', regex=True)]
@@ -171,10 +186,13 @@ def get_rel_company_names(path):
 
 
 
+
 # +
-# exchanges_path= query_params['exchanges_path']
-# exchanges_dict= get_rel_company_names(exchanges_path)
-# exchanges_dict
+# # %%time
+# # exchanges_path= query_params['exchanges_path']
+# # exchanges_dict= get_rel_company_names(exchanges_path)
+# # len(exchanges_dict)
+# [val for list_ in exchanges_dict.values() for val in list_][0:10]
 
 # + tags=[]
 # %%time
@@ -183,18 +201,33 @@ def get_rel_company_names(path):
 # sp_500= query_params['sp500_path'] + 'sp500_list.xlsx'
 # sp500_dict= get_rel_company_names(sp_500)
 exchanges_path= query_params['exchanges_path']
-exchanges_dict= get_rel_company_names(exchanges_path)
+try:
+    exchanges_dict= get_rel_company_names(exchanges_path)
+except:
+    os.chdir("..")
+    os.chdir("..")
 
 def fetch_counter_data(bqclient, table_name, start, sp500_dict):
+    '''
+    Queries the GDELT database in 2day chunks and counts all orgs in the data base for the given
+    time window. Additionally orgs that are matched with a company in the exchanges list or normalized as
+    depicted in the get_rel_company_name function.
+    '''
     word_list= []
+    cache_list= []
     date_range= create_increments(start)
     sp500_names= [val for list_ in sp500_dict.values() for val in list_]
     if len(date_range) == 0:
         end_date= utils.gdelt_date_format(datetime.date.today())
-        # end_date= '20220330153000'
         date_range= [(start, end_date)]
     for start_dates, end_date in tqdm(date_range):
-
+        print(start_dates)
+        # try:
+        #     with open(product['data']+'\\cache\\cache_list.txt', 'r') as convert_file:
+        #         cache_result= convert_file.read().split(' ')
+        # except:
+        #     cache_result= [None]
+        # if start_dates not in cache_result:
         query_string= build_gdelt_query_for_counter(table_name, start_dates, end_date)
         text = (
             bqclient.query(query_string)
@@ -205,7 +238,7 @@ def fetch_counter_data(bqclient, table_name, start, sp500_dict):
                 # API is used by default.
                 create_bqstorage_client=True,
                 ))
-        print('Size: ', len(text))
+        print('Query rows= ', len(text))
         # print('Query: ', datetime.datetime.now())
         # text= re.sub('\\n\d+' , ' ', series.to_string())
         text= text.replace('Organizations', '', limit= 1)
@@ -214,42 +247,63 @@ def fetch_counter_data(bqclient, table_name, start, sp500_dict):
         text= text.str.lower()
         text= text.map(lambda x: re.split(',\d+;?', x))
         # print('Mapping: ', datetime.datetime.now())
-        
-        sp_count=0
+
+        exchange_count=0
         for n, item in text.iteritems():
             for word in item:
                 if word in sp500_names:
                     for key, names_list in sp500_dict.items():
                         if word in names_list:
                             word_list.append(key)
-                            sp_count+=1
+                            exchange_count+=1
                 else:
-                    word_list.append(word)
+                    word_list.append(word) 
+        counts= collections.Counter(word_list)
+        with open(product['data'] + f'\\cache\\org_totals_{start}.txt', 'w') as convert_file:
+             convert_file.write(json.dumps(counts))
+        print(f'Unique words= {len(counts)}')
+        print(f'Number of listed companies instances= {exchange_count}')
+        # with open(product['data'] + '\\org_totals.txt', 'a') as convert_file:
+        #     convert_file.write(' '.join(word_list))
+        # cache_list.append(start_dates)
+        # with open(product['data']+'\\cache\\cache_list.txt', 'w') as convert_file:
+        #     convert_file.write(' '.join(cache_list))
     result= collections.Counter(word_list)
     result= {k:v for k,v in sorted(result.items(), key= lambda x: x[1], reverse=True)}
-    print(sp_count)
+    
     
     return result
 
-counts= fetch_counter_data(client, table_name, start_date, exchanges_dict)
+# counts= fetch_counter_data(client, table_name, start_date, exchanges_dict)
+counts= fetch_counter_data(client, table_name, '20220221141500', exchanges_dict)
 
 
 # + tags=[]
 with open(product['data'] + '\\org_totals.txt', 'w') as convert_file:
      convert_file.write(json.dumps(counts))
+# -
+
+# with open(product['data'] + '\\cache\\org_totals.txt', 'r') as convert_file:
+#      t=json.loads(convert_file.read())
+t['apple inc']
+
+
+# +
+# r= create_increments(start_date)
+# r[-1][0]
 
 # + tags=[]
-# start_date, end_date= ('20220101231500', '20220101233000')
-# query_string= build_gdelt_query_for_counter(table_name, start_date, end_date)
-# text= (
-#             client.query(query_string)
-#                 .result()
-#                 .to_dataframe(
-#                 # Optionally, explicitly request to use the BigQuery Storage API. As of
-#                 # google-cloud-bigquery version 1.26.0 and above, the BigQuery Storage
-#                 # API is used by default.
-#                 create_bqstorage_client=True,
-#                 ))
+start_date, end_date= ('20220101231500', '20220101233000')
+query_string= build_gdelt_query_for_counter(table_name, start_date, end_date)
+text= (
+            client.query(query_string)
+                .result()
+                .to_dataframe(
+                # Optionally, explicitly request to use the BigQuery Storage API. As of
+                # google-cloud-bigquery version 1.26.0 and above, the BigQuery Storage
+                # API is used by default.
+                create_bqstorage_client=True,
+               ))
 
 # + tags=[]
 # x= text.copy().replace('Organizations', '', limit= 1)
@@ -261,3 +315,19 @@ with open(product['data'] + '\\org_totals.txt', 'w') as convert_file:
 # + tags=[]
 # with open(product['data'] + '\\org_totals.txt', 'w') as convert_file:
 #      convert_file.write(json.dumps({'a':'b'}))
+
+# +
+## for reading the cache files and combining and dumping into org_totals.txt
+# folder= 'C:\\Users\\mattd\\OneDrive\\Masters\\SIADS-697 Capstone Project III\\market_watch2\\output\\data\\raw\\cache\\'
+# org_count= collections.Counter()
+# for file in os.listdir(folder):
+#     with open(folder+file) as cache_file:
+#         org_count.update(json.loads(cache_file.read()))
+#         print(len(org_count))
+
+# results= {key : val for key, val in sorted(org_count.items(), key=lambda x: x[1], reverse=True)}
+# with open(product['data'] + '\\org_totals.txt', 'w') as convert_file:
+#      convert_file.write(json.dumps(results))
+# -
+
+
